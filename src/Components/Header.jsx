0ec858416dd5f646f192c90dedec1d084e60d6d1
GhostofUchiha18 <shivanapit8@gmail.com>
2024-12-03 15:47:32 +0545

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,13 +16,13 @@ function Header() {
         <nav className="bg-white dark:bg-gray-900 fixed  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a
-              href="https://flowbite.com/"
+              href="#"
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
               <img
                 src="./images/logo.png"
                 className="h-6"
-                alt="Flowbite Logo"
+                alt="#"
               />
             </a>
             <div className="flex items-center md:order-2 space-x-3 rtl:space-x-reverse">
