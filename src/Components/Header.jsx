149aa6b760e457a850c9e6c69ad33abacfcad339
GhostofUchiha18 <shivanapit8@gmail.com>
2024-12-08 16:28:59 +0545

    changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
     <>
       <section>
         {/* Navbar */}
-        <nav className="bg-white dark:bg-gray-900 fixed  w-full z-20 top-0 le start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white dark:bg-gray-900 fixed  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a
               href="#"
@@ -67,11 +67,14 @@ function Header() {
                   />
                 </svg>
               </button>
+              
             </div>
             <div
               className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
               id="navbar-sticky"
             >
+              <img src="./images/logo.png" className="h-8 absolute left-5" alt="" />
+              
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                   <a
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination"; // You can keep this if you need pagination styles, but it's not necessary anymore
+import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 
 function Home() {
@@ -11,134 +11,75 @@ function Home() {
         <Swiper
           spaceBetween={50}
           slidesPerView={1}
-          modules={[Pagination]} // You can remove this if you don't want pagination behavior at all
+          modules={[Pagination]}
           className="mySwiper"
         >
           <SwiperSlide>
-            <div className="container mx-auto font-minion justify-center text-white font-bold items-center px-4">
+            <div className="container mx-auto font-minion justify-center text-white font-bold items-center">
               <center>
-                <img className=" xl:hidden" src="./images/dan.jpg" alt="" />
-              </center>
-
-              <center>
-                <p className="lg:ml-80 lg:mr-80 mt-4 sm:items-center absolute top-[70%] mr-8 ml-8 xl:hidden">
-                  Nepal, a small landlocked country nestled in the heart of the Himalayas, is known for its unique and vibrant culture. With a history spanning thousands of years, Nepal’s culture is a beautiful blend of ancient traditions, religious diversity, and ethnic variety.
+                <img
+                  className="xl:hidden h-[705px]"
+                  src="./images/art.jpg"
+                  alt="Artwork"
+                />
+                <p className="lg:ml-80 lg:mr-80 mt-4 sm:items-center font-minion absolute top-[82%] xl:hidden">
+                  In this dedicated space, visitors can transform their creative
+                  visions into wearable art pieces that tell their unique
+                  stories.
                 </p>
               </center>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="container mx-auto font-minion justify-center items-center px-4">
-              <img className="h-[800px]" src="./images/dan.jpg" alt="" />
-              <p>
-                Nepal, a small landlocked country nestled in the heart of the
-                Himalayas, is known for its unique and vibrant culture. With a
-                history spanning thousands of years, Nepal’s culture is a
-                beautiful blend of ancient traditions, religious diversity, and
-                ethnic variety. From the towering peaks of Mount Everest to the
-                bustling streets of Kathmandu, Nepal’s culture is as diverse as
-                its landscape. Ethnic and Cultural Diversity Nepal is home to
-                over 125 different ethnic groups, each with its own distinct
-                language, traditions, and way of life. This incredible diversity
-                is reflected in the country’s festivals, rituals, and day-to-day
-                life. Some of the major ethnic groups in Nepal include the
-                Sherpas, Newars, Tharus, Magars, and Tamangs, among many others.
-                Each community brings its own unique cultural practices and
-                beliefs to the rich tapestry of Nepalese culture. Nepal’s
-                diversity is also reflected in its languages, with over 120
-                languages spoken across the country. Nepali, the official
-                language, serves as a unifying force, but many regions continue
-                to speak their native dialects. This linguistic variety adds
-                another layer of richness to the cultural fabric of Nepal.
-                Religion: The Heart of Nepali Culture Religion plays a central
-                role in the culture of Nepal. The country is predominantly
-                Hindu, followed by a significant Buddhist population. In fact,
-                Nepal is the birthplace of Lord Buddha, in Lumbini, making
-                Buddhism an integral part of the nation’s heritage. Hinduism and
-                Buddhism coexist harmoniously, with many religious practices and
-                rituals shared between the two. Temples, stupas, and shrines are
-                scattered throughout the country, serving as places of worship,
-                reflection, and celebration. The Kathmandu Valley, in
-                particular, is home to many UNESCO World Heritage Sites,
-                showcasing the artistic and spiritual importance of these
-                religions. One of the most significant religious festivals is
-                Dashain, celebrated by Hindus to mark the victory of good over
-                evil. During this time, families come together for rituals,
-                feasts, and the exchange of blessings. Another important
-                festival is Tihar, known as the Festival of Lights, where people
-                decorate their homes with oil lamps and celebrate various
-                deities and animals. Festivals: A Celebration of Life Nepal’s
-                festivals are an expression of its vibrant culture. Each
-                festival is deeply rooted in religious and cultural traditions,
-                and they offer a glimpse into the everyday lives of the people.
-                Some of the most prominent festivals in Nepal include: Buddha
-                Jayanti: Celebrating the birth, enlightenment, and death
-                anniversary of Lord Buddha, this festival is particularly
-                significant in the Buddhist community. Indra Jatra: A festival
-                celebrated in Kathmandu with chariot processions, traditional
-                dances, and the display of the living goddess, Kumari. Holi: The
-                festival of colors, celebrated in spring, is enjoyed by people
-                of all ethnic and religious backgrounds, symbolizing unity and
-                the triumph of good over evil. The communal aspect of these
-                festivals strengthens social bonds and keeps cultural practices
-                alive across generations. Traditional Arts and Crafts Nepal is
-                renowned for its traditional arts and crafts, which have been
-                passed down through generations. The intricate wood carvings,
-                thangka paintings, and hand-woven textiles are just a few
-                examples of the country’s artistic heritage. The Newar people,
-                for instance, are famous for their architecture and
-                craftsmanship, particularly in the Kathmandu Valley, where their
-                wooden windows and temples are architectural marvels.
-                Handicrafts, such as pottery, jewelry, and textiles, are made by
-                skilled artisans using methods that have been honed over
-                centuries. These crafts not only reflect the cultural and
-                religious beliefs of the people but also play an important role
-                in the economy, with many items being sold to tourists and
-                exported abroad. Traditional Music and Dance Music and dance
-                hold a special place in Nepali culture. Traditional Nepali music
-                is diverse, with instruments like the madal (drum), sarangi
-                (string instrument), and bansuri (flute) being central to many
-                performances. Music is often intertwined with religious rituals,
-                folk celebrations, and social events. Nepali folk dances, such
-                as the Tamang Selo and the Maruni, are performed during
-                festivals and special occasions. These dances are not only
-                artistic expressions but also serve as a means of storytelling,
-                passing on history, legends, and cultural values to younger
-                generations. Cuisine: A Reflection of Nepal's Cultural Diversity
-                Nepali cuisine is as diverse as its culture. The food is a
-                delightful blend of flavors, influenced by the various ethnic
-                communities and religious practices. Dal Bhat (lentils and
-                rice), served with vegetables and pickles, is the staple food of
-                Nepal and is enjoyed daily by most people. Other popular dishes
-                include momo (dumplings), sel roti (fried rice doughnuts),
-                chatamari (Nepali rice pancakes), and gundruk (fermented leafy
-                greens). The diverse food culture reflects the mix of influences
-                from neighboring India, Tibet, and China, creating a unique
-                culinary experience. The Role of Nature in Culture Nature is
-                deeply interwoven with the cultural and religious practices of
-                the people in Nepal. The majestic Himalayas are considered
-                sacred, with many of the peaks being worshipped by the
-                indigenous communities. The sacred river Bagmati, which flows
-                through Kathmandu, holds significant religious value for Hindus
-                and Buddhists alike. Nepali culture emphasizes respect for
-                nature, and many festivals and rituals are centered around
-                agricultural cycles, such as planting and harvesting. The
-                connection to nature is also reflected in the architecture, with
-                temples, homes, and public spaces often designed to blend
-                harmoniously with the surrounding landscape. Conclusion Nepal’s
-                culture is a beautiful and complex tapestry woven from centuries
-                of history, religion, and tradition. The country’s ethnic
-                diversity, religious harmony, and respect for nature form the
-                foundation of a unique cultural identity that continues to
-                thrive in the modern world. Whether through its festivals, art,
-                music, or cuisine, Nepal’s culture offers a glimpse into the
-                heart and soul of this magnificent nation, where ancient
-                traditions meet the ever-changing present.
+            <div className="container font-minion bg-[#0B192C] justify-center">
+              <img
+                className="xl:hidden h-[505px] w-full"
+                src="./images/art.jpg"
+                alt="Artwork"
+              />
+              <p className="mt-5 text-xl text-center text-white">
+                In this dedicated space, visitors can transform their creative
+                visions into wearable art pieces that tell their unique stories.
+                Whether designing a meaningful pendant, crafting a personalized
+                bracelet, or assembling a one-of-a-kind necklace, each piece
+                becomes a reflection of its creator's style and imagination. The
+                jewelry-making experience caters to all skill levels, providing
+                high-quality materials and expert guidance throughout the
+                creative process. Visitors can select from an array of beads,
+                charms, and findings, combining colors, textures, and patterns
+                to create pieces that resonate with their aesthetic. The
+                studio's approach emphasizes both creativity and craftsmanship,
+                ensuring that each finished piece is both beautiful and durable.
               </p>
             </div>
           </SwiperSlide>
         </Swiper>
       </section>
+
+
+      {/* for the desktop */}
+
+      {/* <div class="hidden sm:block max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div> */}
+
+
     </>
   );
 }
