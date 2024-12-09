27df57534dfe993127e9d52e600b426df40593b5
GhostofUchiha18 <shivanapit8@gmail.com>
2024-12-09 14:51:04 +0545

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
                   src="./images/art.jpg"
                   alt="Artwork"
                 />
-                <p className="lg:ml-80 lg:mr-80 mt-4 sm:items-center font-minion absolute top-[82%] xl:hidden">
+                <p className="lg:ml-80 lg:mr-80 mt-4 sm:items-center font-minion absolute top-[84%] xl:hidden bg-black bg-opacity-40">
                   In this dedicated space, visitors can transform their creative
                   visions into wearable art pieces that tell their unique
                   stories.
@@ -37,7 +37,8 @@ function Home() {
                 src="./images/art.jpg"
                 alt="Artwork"
               />
-              <p className="mt-5 text-xl text-center text-white">
+              <h4 className="text-xl font-bold font-graphik text-white mt-4 text-center">HANDMADE JEWELRY MAKING</h4>
+              <p className="mt-2 mb-4 w-full text-xs text-center text-white xl:hidden px-3">
                 In this dedicated space, visitors can transform their creative
                 visions into wearable art pieces that tell their unique stories.
                 Whether designing a meaningful pendant, crafting a personalized
@@ -56,30 +57,89 @@ function Home() {
         </Swiper>
       </section>
 
-
-      {/* for the desktop */}
-
-      {/* <div class="hidden sm:block max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
-    <div class="p-5">
+      <div className="flex">
+      <div class="w-72 hidden sm:block bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-24 mt-20 ml-20">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <img class="rounded-t-lg h-72 w-full" src="./images/art.jpg" alt="" />
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-        </p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-graphik font-bold tracking-tight text-gray-900 dark:text-white">
+              Handmade Jewelry Making
+            </h5>
+          </a>
+          <p class="mb-3 font-minion font-normal text-gray-700 dark:text-gray-400">
+          In this dedicated space, visitors can transform their creative
+          visions into wearable art pieces that tell their unique
+          stories.
+          </p> 
+
+        </div>
+      </div>
+
+      <div class="w-72 hidden sm:block bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-24 mt-20 ml-5">
+        <a href="#">
+          <img class="rounded-t-lg h-72 w-full" src="./images/art.jpg" alt="" />
         </a>
-    </div>
-</div> */}
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-graphik font-bold tracking-tight text-gray-900 dark:text-white">
+              Handmade Jewelry Making
+            </h5>
+          </a>
+          <p class="mb-3 font-minion font-normal text-gray-700 dark:text-gray-400">
+          In this dedicated space, visitors can transform their creative
+          visions into wearable art pieces that tell their unique
+          stories.
+          </p> 
+
+        </div>
+      </div>
+
+      <div class="w-72 hidden sm:block bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-24 mt-20 ml-5">
+        <a href="#">
+          <img class="rounded-t-lg h-72 w-full" src="./images/art.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-graphik font-bold tracking-tight text-gray-900 dark:text-white">
+              Handmade Jewelry Making
+            </h5>
+          </a>
+          <p class="mb-3 font-minion font-normal text-gray-700 dark:text-gray-400">
+          In this dedicated space, visitors can transform their creative
+          visions into wearable art pieces that tell their unique
+          stories.
+          </p> 
+
+        </div>
+      </div>
+
+      <div class="w-72 hidden sm:block bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-24 mt-20 ml-5">
+        <a href="#">
+          <img class="rounded-t-lg h-72 w-full" src="./images/art.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-graphik font-bold tracking-tight text-gray-900 dark:text-white">
+              Handmade Jewelry Making
+            </h5>
+          </a>
+          <p class="mb-3 font-minion font-normal text-gray-700 dark:text-gray-400">
+          In this dedicated space, visitors can transform their creative
+          visions into wearable art pieces that tell their unique
+          stories.
+          </p> 
+
+        </div>
+      </div>
 
 
+      </div>
+
+
+
+      
     </>
   );
 }
